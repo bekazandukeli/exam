c053c38524bad42107a1d52369ae6d9ac26664f3
bekazandukeli <bekazandukeli@gmail.com>
2022-01-05 13:18:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Data from './Data.json';
+import Alert from 'react-bootstrap/Alert';
+import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const filteredDataData = Data.questions.filter((q) =>
+    q.question.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div style={styles.searchContainer}>
+        <FormControl
+          placeholder="Question"
+          aria-label="Question"
+          aria-describedby="basic-addon1"
+          style={styles.searchBox}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {/* <Button variant="primary">Search</Button> */}
+      </div>
+      {filteredDataData.map((q) => (
+        <Card style={styles.card} key={q.question}>
+          <Card.Body>
+            <Alert variant="dark">{q.question}</Alert>
+            {q.answer && <Alert variant="success">{q.answer}</Alert>}
+            {q.explanation && <Alert variant="warning">{q.explanation}</Alert>}
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  container: {
+    margin: '25px 25px',
+  },
+  searchContainer: {
+    marginBottom: '40px',
+    display: 'flex',
+  },
+  searchBox: {
+    marginRight: '20px',
+  },
+  card: {
+    marginBottom: '2rem',
+    backgroundColor: '#eee',
+  },
+};
